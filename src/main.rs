6e752dca41d9d8a512641c1c366743f875a4c61f
Mathijs Frank <33691132+mathijs28498@users.noreply.github.com>
2022-07-17 21:31:06 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+// TODO: Implement 2d raytracing
+// TODO: Draw using vulkano with a texture in a quad
+//          This will make ui libraries work
+// TODO: Make image buffer gpu only
+
+#![allow(unused_imports)]
+
+use bevy::{app::AppExit, prelude::*, window::WindowResizeConstraints};
+use bevy_pixels::prelude::*;
+use nalgebra::*;
+use rand::prelude::*;
+
+mod vulkano_backend;
+use vulkano::pipeline::compute;
+use vulkano_backend::compute_device::{WIDTH, HEIGHT, BUFFER_SIZE, self, ComputeDevice};
+
+use rand::Rng;
+
+
+// #[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+// enum AppStage {
+//     DrawBackground,
+//     DrawObjects,
+// }
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Hello Bevy Pixels".to_string(),
+            width: WIDTH as f32,
+            height: HEIGHT as f32,
+            resize_constraints: WindowResizeConstraints {
+                min_width: WIDTH as f32,
+                min_height: HEIGHT as f32,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert_resource(
+            compute_device::ComputeDevice::new()
+        )
+        // .insert_resource(GameObjects {
+        //     balls: vec![
+        //         Ball::new(Vector3::new(200., 200., 10.), 50., [255, 0, 0, 255]),
+        //         Ball::new(Vector3::new(150., 150., 5.), 50., [0, 255, 0, 255]),
+        //     ],
+        // })
+        .insert_resource(PixelsOptions {
+            width: WIDTH,
+            height: HEIGHT,
+        })
+        .add_startup_system(init_compute_device_system)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PixelsPlugin)
+        .add_system(exit_on_escape_system)
+        // .add_stage_after(
+        //     PixelsStage::Draw,
+        //     AppStage::DrawBackground,
+        //     SystemStage::parallel(),
+        // )
+        // .add_stage_after(
+        //     AppStage::DrawBackground,
+        //     AppStage::DrawObjects,
+        //     SystemStage::parallel(),
+        // )
+        .add_system(draw_objects_system)
+        // .add_system_to_stage(AppStage::DrawBackground, draw_background_system)
+        // .add_system_to_stage(AppStage::DrawObjects, draw_objects_system)
+        .run();
+}
+
+fn exit_on_escape_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_events: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_events.send(AppExit);
+    }
+}
+
+fn init_compute_device_system(
+    mut compute_device: ResMut<ComputeDevice>
+) {
+    compute_device.init();
+    compute_device.create_buffers();
+    compute_device.create_pipeline();
+}
+
+fn draw_objects_system(
+    mut pixels_resource: ResMut<PixelsResource>,
+    mut compute_device: ResMut<ComputeDevice>
+    // game_objects: Res<GameObjects>,
+) {
+
+
+    let frame = pixels_resource.pixels.get_frame();
+    compute_device.execute();
+    compute_device.await_future();
+    let frame_buffer = compute_device.fill_u8().unwrap();
+    for (i, pix) in frame_buffer.iter().enumerate() {
+        frame[i] = *pix;
+    }
+}
+
+fn get_sq_dist(v0: &Vector3<f32>, v1: &Vector3<f32>) -> f32 {
+    let x = v0 - v1;
+    return x.x.powi(2) + x.y.powi(2) + x.z.powi(2);
+}
---
+++ src/vulkano_backend/compute_device.rs
@@ -0,0 +1,214 @@
+use std::{borrow::BorrowMut, cell::RefMut, sync::Arc};
+
+use vulkano::{
+    buffer::{BufferContents, BufferUsage, CpuAccessibleBuffer, ImmutableBuffer},
+    command_buffer::{
+        AutoCommandBufferBuilder, CommandBufferExecFuture, CommandBufferUsage,
+        PrimaryAutoCommandBuffer,
+    },
+    descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet},
+    device::{
+        physical::{PhysicalDevice, PhysicalDeviceType},
+        Device, DeviceCreateInfo, DeviceExtensions, Queue, QueueCreateInfo,
+    },
+    instance::Instance,
+    pipeline::{ComputePipeline, Pipeline, PipelineBindPoint},
+    sync::{self, FenceSignalFuture, GpuFuture, NowFuture},
+};
+
+use nalgebra_glm as glm;
+
+
+pub const WIDTH: u32 = 480;
+pub const HEIGHT: u32 = 480;
+pub const BUFFER_SIZE: usize = (WIDTH * 4) as usize;
+pub const WORK_GROUP_SIZE: i32 = WIDTH as i32 * HEIGHT as i32 / 256;
+
+pub struct ComputeDevice {
+    device: Option<Arc<Device>>,
+    queue: Option<Arc<Queue>>,
+    pixel_buffer: Option<Arc<CpuAccessibleBuffer<[u32]>>>,
+    command_buffer: Option<PrimaryAutoCommandBuffer>,
+    pipeline: Option<Arc<ComputePipeline>>,
+    set: Option<Arc<PersistentDescriptorSet>>,
+    buffer_u32: Option<Vec<u32>>,
+    future: Option<FenceSignalFuture<CommandBufferExecFuture<NowFuture, PrimaryAutoCommandBuffer>>>,
+}
+
+impl ComputeDevice {
+    pub fn new() -> Self {
+        ComputeDevice {
+            device: None,
+            queue: None,
+            pixel_buffer: None,
+            command_buffer: None,
+            pipeline: None,
+            set: None,
+            buffer_u32: None,
+            future: None,
+        }
+    }
+
+    pub fn init(&mut self) {
+        let instance = Instance::new(Default::default()).unwrap();
+
+        let device_extensions = DeviceExtensions {
+            khr_storage_buffer_storage_class: true,
+            ..DeviceExtensions::none()
+        };
+        let (physical_device, queue_family) = PhysicalDevice::enumerate(&instance)
+            .filter(|&p| p.supported_extensions().is_superset_of(&device_extensions))
+            .filter_map(|p| {
+                p.queue_families()
+                    .find(|&q| q.supports_compute())
+                    .map(|q| (p, q))
+            })
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+            })
+            .unwrap();
+
+        println!(
+            "Using device: {} (type: {:?})",
+            physical_device.properties().device_name,
+            physical_device.properties().device_type
+        );
+
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                enabled_extensions: physical_device
+                    .required_extensions()
+                    .union(&device_extensions),
+                queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+        let queue = queues.next().unwrap();
+
+        self.device = Some(device);
+        self.queue = Some(queue);
+    }
+
+    // TODO: Look if the initialization can be improved of the buffer
+    pub fn create_buffers(&mut self) {
+        // TODO: Change to CpuBufferPool
+        let pixel_buffer = {
+            let data_iter = (0..(WIDTH * HEIGHT) as usize).map(|n| 0_u32);
+            CpuAccessibleBuffer::from_iter(
+                self.device.as_ref().unwrap().clone(),
+                BufferUsage {
+                    storage_buffer: true,
+                    ..BufferUsage::none()
+                },
+                false,
+                data_iter,
+            )
+            .unwrap()
+        };
+
+        self.pixel_buffer = Some(pixel_buffer);
+    }
+
+    pub fn create_pipeline(&mut self) {
+        let pipeline = {
+            mod cs {
+                vulkano_shaders::shader! {
+                    ty: "compute",
+                    path: "src/shaders/basic_shader.comp"
+                }
+            }
+            let shader = cs::load(self.device.as_ref().unwrap().clone()).unwrap();
+
+            ComputePipeline::new(
+                self.device.as_ref().unwrap().clone(),
+                shader.entry_point("main").unwrap(),
+                &(),
+                None,
+                |_| {},
+            )
+            .unwrap()
+        };
+
+        let layout = pipeline.layout().set_layouts().get(0).unwrap();
+
+        let set = PersistentDescriptorSet::new(
+            layout.clone(),
+            [
+                WriteDescriptorSet::buffer(0, self.pixel_buffer.as_ref().unwrap().clone()),
+            ],
+        )
+        .unwrap();
+
+        self.pipeline = Some(pipeline);
+        self.set = Some(set);
+    }
+
+    pub fn execute(&mut self) {
+        let mut builder = AutoCommandBufferBuilder::primary(
+            self.device.as_ref().unwrap().clone(),
+            self.queue.as_ref().unwrap().family(),
+            CommandBufferUsage::MultipleSubmit,
+        )
+        .unwrap();
+
+        builder
+            .bind_pipeline_compute(self.pipeline.as_ref().unwrap().clone())
+            .bind_descriptor_sets(
+                PipelineBindPoint::Compute,
+                self.pipeline.as_ref().unwrap().layout().clone(),
+                0,
+                self.set.as_ref().unwrap().clone(),
+            )
+            // .push_constants(
+            //     self.pipeline.as_ref().unwrap().layout().clone(),
+            //     0,
+            //     push_constants,
+            // )
+            .dispatch([WORK_GROUP_SIZE as u32, 1, 1])
+            .unwrap();
+
+        let command_buffer = builder.build().unwrap();
+
+        let future = sync::now(self.device.as_ref().unwrap().clone())
+            .then_execute(self.queue.as_ref().unwrap().clone(), command_buffer)
+            .unwrap()
+            .then_signal_fence_and_flush()
+            .unwrap();
+
+        self.future = Some(future);
+    }
+
+    pub fn await_future(&mut self) -> bool {
+        let future = match self.future.as_ref() {
+            Some(f) => f,
+            None => return false,
+        };
+        future.wait(None).unwrap();
+
+        let data_buffer_content = self.pixel_buffer.as_ref().unwrap().read().unwrap();
+        self.buffer_u32 = Some(data_buffer_content.to_owned());
+        true
+    }
+
+    pub fn fill_u8(&self) -> Result<&[u8; BUFFER_SIZE], &str> {
+        let buffer = match self.buffer_u32.as_ref() {
+            Some(b) => b,
+            None => return Err("Command has never executed, so buffer is None"),
+        };
+
+        let mut last_index = 0;
+
+        let res: &[u8; BUFFER_SIZE] = buffer
+            .as_bytes()
+            .try_into()
+            .expect("Failed to turn slice into full length");
+
+        Ok(res)
+    }
+}
